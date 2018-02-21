61645f45787c88d9cbd42d8c87bd68d9d05ef4af
shagundave <30891188+shagundave@users.noreply.github.com>
2018-02-21 14:00:26 +0530

    Update awsMarketPlaceEcommerceAnalytics.py

--- awsMarketPlaceEcommerceAnalytics.py
+++ awsMarketPlaceEcommerceAnalytics.py
@@ -2,13 +2,13 @@
 # 
 import datetime 
 import boto3
+def lambda_handler(event, context):
+    client = boto3.client('marketplacecommerceanalytics')
 
-client = boto3.client('marketplacecommerceanalytics')
-
-response = client.start_support_data_export(dataSetType='test_customer_support_contacts_data',
-    fromDate=datetime.datetime.now() - datetime.timedelta(days=7),
-    roleNameArn='arn:aws:iam::212445883776:role/MarketplaceCommerceAnalyticsRole',
-    destinationS3BucketName='intuzcloud-ami-support',
-    destinationS3Prefix='intuzcloud-ami-customer',
-    snsTopicArn='arn:aws:sns:us-west-2:212445883776:intuzcloud-ami-support-sns'
-    ) 
+    response = client.start_support_data_export(dataSetType='test_customer_support_contacts_data',
+        fromDate=datetime.datetime.now() - datetime.timedelta(days=7),
+        roleNameArn='arn:aws:iam::xxxxxxxxx:role/MarketplaceCommerceAnalyticsRole',
+        destinationS3BucketName='cloud-ami-support',
+        destinationS3Prefix='cloud-ami-customer-list',
+        snsTopicArn='arn:aws:sns:us-west-2:xxxxxxxxx:cloud-ami-support-sns'
+        ) 
