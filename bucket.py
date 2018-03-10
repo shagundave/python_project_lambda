68fc6f36c6ac6ea75deb868f0d45dbcdd15e941a
shagundave <shagun.dave@yahoo.com>
2018-03-11 00:36:36 +0530

    updated code

--- bucket.py
+++ bucket.py
@@ -1,23 +1,23 @@
 # Create bucket
 import boto3
-#s3 = boto3.resource('s3')
+s3 = boto3.resource('s3')
 #response = s3.create_bucket(Bucket='testlambda-abc')
 #print(response.name)
 
 # Bucket List
-s3 = boto3.resource('s3')
+#s3 = boto3.resource('s3')
 #for bucket in s3.buckets.all():
 #        print(bucket.name)
 
 # Delete Bucket
-#db = s3.Bucket('testlambda-abc')
-#dlt = db.delete()
-#print(dlt)
+db = s3.Bucket('testlambda-abc')
+dlt = db.delete()
+print(dlt)
 
 ## Put object in Bucket
 #a = s3.Bucket('testlambda-xyz').upload_file('/tmp/hello.txt', 'hello.txt')
 #print(a)
 # check object
-your_bucket = s3.Bucket('testlambda-xyz')
-for s3_file in your_bucket.objects.all():
-    print(s3_file.key)
+#your_bucket = s3.Bucket('shagunbitcanopybills')
+#for s3_file in your_bucket.objects.all():
+#    print(s3_file.key)
---
+++ database/pgsql.py
@@ -0,0 +1,19 @@
+###Connection postgresql database
+import psycopg2
+import datetime
+import csv
+
+myConnection = psycopg2.connect(database="testdb",user="sammy",password="test@123",host="54.89.251.242",port="5432")
+
+print("Database is now connect")
+
+cur = myConnection.cursor()
+cur.execute("select * from task natural join time;")
+#ver = list(cur.fetchone())
+
+with open("/root/script/out.csv", "w", newline='') as csv_file:  # Python 3 version
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow([i[0] for i in cur.description]) # write headers
+    csv_writer.writerows(cur)
+
+print("Writing complete")
